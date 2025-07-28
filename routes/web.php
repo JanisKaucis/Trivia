4fd4685f965fab178efa9f225b6c4bcbee38693d
JanisKaucis <janiskaucis@gmail.com>
2025-07-28 21:39:15 +0300

    MOD: refactor, move QuestionController.php to TriviaController.php and delete QuestionController.php

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Inertia\Inertia;
 
 Route::get('/', [TriviaController::class, 'index'])->name('home');
 
-Route::get('question', [QuestionController::class, 'getTrivia'])->name('question');
-Route::post('answer', [QuestionController::class, 'postAnswer'])->name('answer');
+Route::get('question', [TriviaController::class, 'getTrivia'])->name('question');
+Route::post('answer', [TriviaController::class, 'postAnswer'])->name('answer');
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
